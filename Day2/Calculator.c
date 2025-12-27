0c2eec295bc9d5c24af47acfad8bd84622c8d5e8
vmtech-support <info@vmtechnologiesllc.com>
2025-12-27 10:42:32 +0530

    ignored a.out

--- Day2/Calculator.c
+++ Day2/Calculator.c
@@ -1,18 +1,19 @@
 #include<stdio.h>
 
+int add(int, int);
+int mul(int,int);
+int div(int,int);
+int sub(int,int);
+void menu();
+
 int main(){
     float num1, num2;
     char op;
 
+    menu();
+
     while(op != '#'){
-        printf(" ===== Calculator Menu ========\n");
-        printf("+ for Addtion\n");
-        printf("- for Subtraction\n");
-        printf("* for Multiplication\n");
-        printf("# for Exit\n");
-
-        printf("/ for Division\n");
-
+        
         printf("Enter the two value: \n");
         scanf("%f %f",&num1,&num2);
 
@@ -23,20 +24,16 @@ int main(){
 
         switch(op){
             case '+':
-                result = num1 + num2;
+                result = add(num1,num2);
                 break;
             case '-':
-                result = num1 - num2;
+                result = sub(num1,num2);
                 break;
             case '*':
-                result = num1 * num2;
+                result = mul(num1,num2);
                 break;
             case '/':
-                if(num2 == 0){
-                    printf("cannot divided by 0");
-                    break;
-                }
-                result = num1 / num2;
+                result = div(num1,num2);
                 break;
             default:
                 printf("Invalid operation");
@@ -47,4 +44,34 @@ int main(){
 
         printf("\n");
     }
+}
+
+void menu(){
+    printf(" ===== Calculator Menu ========\n");
+    printf("+ for Addtion\n");
+    printf("- for Subtraction\n");
+    printf("* for Multiplication\n");
+    printf("/ for Division\n");
+    printf("# for Exit\n");
+
+}
+
+int add(int a,int b){
+    return a+b;
+}
+
+int sub(int a,int b){
+    return a - b;
+}
+
+int mul(int a,int b){
+    return a * b;
+}
+
+int div(int a,int b){
+    if(b == 0){
+        printf("cannot divided by 0");
+        return -1;
+    }
+    return a / b;
 }
